708af28c47165bc5b39592440772b7947b600863
thureson <tom.thureson@gmail.com>
2018-07-31 13:45:17 +0300

    Fix problems about environment-variables

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,8 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
+const secretEnv = require('./secret.env')
 
-module.exports = merge(prodEnv, {
+module.exports = merge(prodEnv, secretEnv, {
   NODE_ENV: '"development"'
 })
